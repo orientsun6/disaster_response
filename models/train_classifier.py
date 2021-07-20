a1adaa928b4469331ed500e2c8660d17afea2168
Charles Liu <charles.dongyangliu@gmail.com>
2021-07-19 22:39:57 -0400

    added model

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,77 @@
 import sys
+# import libraries
+from sqlalchemy import create_engine
+import pandas as pd
+import numpy as np
+import pickle
 
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+
+import re
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import GridSearchCV
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.datasets import make_multilabel_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.datasets import make_multilabel_classification
+from sklearn.metrics import classification_report
+
+
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
 
 def load_data(database_filepath):
-    pass
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    df = pd.read_sql('SELECT * FROM disaster_respone', engine)
+    X = df['message']
+    y = df.iloc[:, 4:]
+    return X, y, y.columns.tolist()
 
 
 def tokenize(text):
-    pass
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+    return clean_tokens
 
 
 def build_model():
-    pass
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(DecisionTreeClassifier()))
+    ])
+    parameters = {
+    'vect__ngram_range': ((1, 1), (1, 2)),
+    'vect__max_df': (0.5, 0.75),
+    'vect__max_features': (None, 5000),
+    'tfidf__use_idf': (True, False),
+    'clf__estimator__max_depth': [2, 5, 10]
+    }
 
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    y_pred = model.predict(X_test)
+    for i, category in enumerate(category_names):
+        print(category)
+        print(classification_report(Y_test[category], y_pred[:, i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    with open (model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
@@ -27,13 +80,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
